a2735d60ec6cf21c5cb04d575f6c4f86b398d74c
Iggna <iggnaa@gmail.com>
2021-10-08 16:07:41 -0300

    borrado console logs

--- sw.js
+++ sw.js
@@ -1,20 +1,14 @@
-// On install the application shell cached
 self.addEventListener('install', function(event) {
     event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
     return cache.add('index.html', 'app.js', 'images.js', './static/getImages.js', './images')
-
-    // if you have css file and app.js files
-    // please add here as well to be cached! we havent added as we have a simple app
     })
   );
 });
     // with request network
     self.addEventListener('fetch', function(event) {
     event.respondWith(
-    // Try the cache
     caches.match(event.request).then(function(response) {
-    // return it if there is a response, or else fetch again
     return response || fetch(event.request);
     })
   );
